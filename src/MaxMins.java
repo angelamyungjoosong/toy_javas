603a536c3dbde705f22f9a602664288593d18962
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-31 14:19:57 +0900

    min 작업 완료

---
+++ src/MaxMins.java
@@ -0,0 +1,45 @@
+package src;
+
+import java.util.Scanner;
+
+public class MaxMins
+
+{
+    public int min(int first, int second) {
+        int min = 0;
+        try {
+
+            if (first < second) {
+                min = first;
+            }
+           else if (second > first) {
+             min = second;
+           }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int first = 0;
+            int second = 0;
+            Scanner myObj = new Scanner(System.in);
+
+           
+            first = myObj.nextInt();
+            second = myObj.nextInt();
+            MaxMins maxmins = new MaxMins();
+            int minresult = maxmins.min(first, second);
+            System.out.println("min:");
+            System.out.println(minresult);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+
+}
