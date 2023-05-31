d8199c7401fcc28e375543eae6a8c834132bc99b
estskyway <estskyway@gmail.com>
2023-05-31 14:58:29 +0900

    5/31 2차 수정

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -47,21 +47,21 @@ public class MaxMins {
 
     public static void main(String[] args) {
         try {
-            int first = 0;
+            int first = 0;  //변수값 first second 설정
             int second = 0;
-            Scanner myObj = new Scanner(System.in);
+            Scanner myObj = new Scanner(System.in); //스캐너에 변수 이름 설정
 
            
-            first = myObj.nextInt();
+            first = myObj.nextInt();    //두 변수에 입력창 설정
             second = myObj.nextInt();
             
             MaxMins maxmins = new MaxMins();
-            int minresult = maxmins.min(first, second);
-            System.out.println("min:"+ minresult);
+            int minresult = maxmins.min(first, second); //함수에 두 변수값을 적용
+            System.out.println("min:"+ minresult); //함수에 대입한 결과값을 min로 출력
 
             MaxMins mexMins = new MaxMins();
-            int maxresult = mexMins.max(first, second); 
-            System.out.println("max:"+maxresult);
+            int maxresult = mexMins.max(first, second);  
+            System.out.println("max:"+maxresult);   //함수에 대입한 결과값을 max로 출력
 
         } catch (Exception e) {
             // TODO: handle exception
